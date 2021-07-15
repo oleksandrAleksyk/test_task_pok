53c05521a1ea99e86cc657f514f5c760fa5c3f96
unknown <oleksandr.aleksyk@gmail.com>
2021-07-15 15:45:41 +0300

    Updates

--- src/common/enums/stat-enum.ts
+++
@@ -1,8 +0,0 @@
-export enum StatsOrder {
-    HP = 0,
-    ATTACK=1,
-    DEFENCE=2,
-    SPECIAL_ATTACK = 3,
-    SPECIAL_DEFENCE=4,
-    SPEED=5
-}
--- src/components/header/Header.tsx
+++
@@ -1,6 +0,0 @@
-import './Header.scss';
-export const Header = ()=>(
-    <header className={'header'}>
-        <h1 onClick={()=>{window.location.reload()}}>PokeDex</h1>
-    </header>
-)
